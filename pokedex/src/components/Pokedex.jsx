7505ba28d311991c611152c14205e7835f7b0cb1
CSR-Maroli <marolicesar@gmail.com>
2024-06-04 10:57:02 -0300

    comit

--- pokedex/src/components/Pokedex.jsx
+++ pokedex/src/components/Pokedex.jsx
@@ -1,32 +1,69 @@
-import { useState, useEffect } from "react"
+import React, {useState, useEffect} from "react";
+import style, {styled} from 'styled-components'
+import {motion} from 'framer-motion'
+
+const Spinner = style (motion.div)`
+  width: 50px;
+  height: 50px;
+  border: 4px solid black;
+  border-radius: 50%;
+  border-top: 4px solid red;
+  display: insline-block;
+`;
+
+const Container = style.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
 
 
 export default function Pokedex(){
  const [id, setId] = useState(1);
  const [pokemon, setPokemon] = useState(null);
+ const [carregando, setCarregando] = useState(true);
 
 
 const fetchData = async () => { 
+    setCarregando(true);
 // função assincrona para encontrar dados e conectar na API
     try{
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`) 
         const data = await response.json();
-        setPokemon(data)
+        setTimeout(() => {
+            setPokemon(data);
+            setCarregando(false);
+        },500);
     }catch(error){
         console.error(error);
+        setCarregando(false);
     }
- }
+ };
 
     useEffect(() => {
         fetchData()
-    }), [id]
+    }, [id]);
 
     const nextPokemon = () =>{
         setId(id+1)
     }
+    const anteriorPokemon = () =>{
+        setId(id-1)
+    }
     
     return(
-        <div>
+        <>
+        <Container>
+          {carregando ? (
+            <Spinner
+            animate={{rotate: 360}}
+            transition={{duration: 0.5, repeat: Infinity, ease: 'linear'}}
+          >
+          </Spinner>
+          ):(
+            <div>
                 {pokemon && (
           
             <div className="pokemon">
@@ -35,9 +72,17 @@ const fetchData = async () => {
                 <p>Peso: {pokemon.weight}</p>
                 <img className="pokemon-img" src={pokemon.sprites.front_default} alt="Pokemon" />
                 <button onClick={nextPokemon}>Próximo</button>
+                <button onClick={anteriorPokemon}>Anterior</button>
             </div>
             )}
         </div>
+          )
+}
+
+        </Container>
+        
+        
+        </>
         
     )
 }
